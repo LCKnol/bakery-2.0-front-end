344c7e1cac4b5cab2a6db9419f0d28870606fe63
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-15 12:26:44 +0200

    Added navbar to app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
 import {HttpClientModule} from "@angular/common/http";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginComponent, HttpClientModule],
+    imports: [RouterOutlet, LoginComponent, HttpClientModule, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
