ab37dcd91ab9490aab5808d8914dcb66d0420a91
Eduardo-ict <ej.vanbenthum@student.han.nl>
2024-04-16 12:31:19 +0200

    Update dashboard.service.ts

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -2,19 +2,21 @@ import {Injectable} from '@angular/core';
 import {DashboardsDto} from '../dto/dashboardsDto';
 import {HttpClient, HttpParams} from '@angular/common/http';
 import {firstValueFrom} from "rxjs";
+import {CookieService} from "ngx-cookie-service";
 
 @Injectable({
   providedIn: 'root'
 })
  export class DashboardService {
 
-  constructor(private httpClient: HttpClient) {
+  constructor(private httpClient: HttpClient, private cookieService: CookieService) {
   }
 
   public async getDashboards(): Promise<DashboardsDto> {
 
     const endpointUrl = 'http://localhost:8080/dashboards';
-    const params = new HttpParams().set('token', "test");
+
+    const params = new HttpParams().set('token', this.cookieService.get('token'));
 
     try {
       return await firstValueFrom(this.httpClient.get<DashboardsDto>(endpointUrl, {params: params}));
