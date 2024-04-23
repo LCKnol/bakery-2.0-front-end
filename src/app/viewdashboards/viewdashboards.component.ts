5c681c377717e5daabf0e746dc78611afb2a14f1
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-04-23 12:10:28 +0200

    🐛 Fixed add button styling not showing

--- src/app/viewdashboards/viewdashboards.component.ts
+++ src/app/viewdashboards/viewdashboards.component.ts
@@ -4,7 +4,7 @@ import {NavbarComponent} from '../navbar/navbar.component'
 import {DashboardCollection} from '../dto/dashboardCollection'
 import  {DashboardService} from  "../services/dashboard.service";
 import {CommonModule, NgOptimizedImage} from '@angular/common';
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {MatToolbar} from "@angular/material/toolbar";
 import {RouterLink} from "@angular/router";
 import {MatFormField} from "@angular/material/form-field";
@@ -19,7 +19,7 @@ import {MatCard} from "@angular/material/card";
 @Component({
   selector: 'app-viewdashboards',
   standalone: true,
-  imports: [DashboardCardComponent, NavbarComponent, CommonModule, MatButton, MatToolbar, NgOptimizedImage, RouterLink, MatFormField, MatInput, MatIcon, MatCard],
+  imports: [DashboardCardComponent, NavbarComponent, CommonModule, MatButton, MatToolbar, NgOptimizedImage, RouterLink, MatFormField, MatInput, MatIcon, MatCard, MatIconButton],
   templateUrl: './viewdashboards.component.html',
   styleUrl: './viewdashboards.component.css'
 })
