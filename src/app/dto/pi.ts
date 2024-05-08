7c13236ed89f36a674150b3dcc24a822e7ad6503
koenhs <koenhs@gmail.com>
2024-05-08 11:02:55 +0200

    ✨Rooms are now exclusively selectable from existing rooms.

--- src/app/dto/pi.ts
+++ src/app/dto/pi.ts
@@ -4,5 +4,5 @@ export interface Pi {
   macAddress: string;
   status: string;
   dashboardName: string;
-  roomno: string;
+  roomNo: string;
 }
--- src/app/init-pi/init-pi.component.ts
+++ src/app/init-pi/init-pi.component.ts
@@ -1,16 +1,22 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
+import {Component} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {MatFormField, MatLabel} from "@angular/material/form-field";
 import {Router} from "@angular/router";
-import {MatSnackBar} from "@angular/material/snack-bar";
 import {MatButton} from "@angular/material/button";
 import {MatInput} from "@angular/material/input";
 import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {MatIcon} from "@angular/material/icon";
 import {GeneralService} from "../services/general.service";
 import {Pi} from "../dto/pi";
-import { ActivatedRoute } from '@angular/router';
 import {PiService} from "../services/pi.service";
+import {RoomService} from "../services/room.service";
+import {MatOption} from "@angular/material/autocomplete";
+import {MatSelect} from "@angular/material/select";
+import {RoomCollection} from "../dto/roomCollection";
+import {RoomDto} from "../dto/roomDto";
+import {NgForOf} from "@angular/common";
+
+
 
 
 @Component({
@@ -26,6 +32,9 @@ import {PiService} from "../services/pi.service";
     MatCardContent,
     MatCardHeader,
     MatIcon,
+    MatOption,
+    MatSelect,
+    NgForOf,
   ],
   templateUrl: './init-pi.component.html',
   styleUrl: './init-pi.component.css'
@@ -33,14 +42,35 @@ import {PiService} from "../services/pi.service";
 export class InitPiComponent {
   addInitPiForm: FormGroup = new FormGroup({
     name: new FormControl(''),
-    roomno: new FormControl('')
+    roomNo: new FormControl('')
   });
-
   macAddress: string;
+  rooms: RoomDto[] = []
 
-  constructor(private router: Router, private piService: PiService, private generalService: GeneralService) {
+
+
+  constructor(
+    private router: Router,
+    private piService: PiService,
+    private generalService: GeneralService,
+    private roomService: RoomService,
+
+  ) {
+
     const navigation = this.router.getCurrentNavigation();
-    this.macAddress = navigation?.extras.state?.['data']
+    this.macAddress = navigation?.extras.state?.['data'];
+
+    // Fetch room numbers from the backend API
+    this.fetchRooms();
+  }
+
+
+
+  fetchRooms() {
+    // Make an HTTP GET request to your backend API to fetch room numbers
+    this.roomService.getAllRooms().then((rooms: RoomCollection) => {
+      this.rooms = rooms.rooms
+    });
   }
 
   submitInitPiForm() {
@@ -50,7 +80,7 @@ export class InitPiComponent {
       macAddress: this.macAddress,
       status: '',
       dashboardName: '',
-      roomno: this.addInitPiForm.value.roomno ?? ''
+      roomNo: this.addInitPiForm.value.roomNo.roomNo ?? ''
     };
 
     this.piService.initPi(pi).then(_ => {
