c98f2d5e1e8f928fcec9bd5cec36296c3d4f1a0c
Bastiaan Hopman <BJ.Hopman@student.han.nl>
2024-05-24 16:41:14 +0200

    ✨ Updated edit pi

--- src/app/editpi/editpi.component.ts
+++ src/app/editpi/editpi.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, Inject} from '@angular/core';
 import { MatSelect, MatOption } from '@angular/material/select';
 
 import { ActivatedRoute, Router } from '@angular/router';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { MatButton, MatMiniFabButton } from '@angular/material/button';
 import { MatCard, MatCardContent, MatCardHeader } from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
@@ -14,7 +14,12 @@ import { Pi } from '../dto/pi';
 import {RoomDto} from "../dto/roomDto";
 import {RoomService} from "../services/room.service";
 import {RoomCollection} from "../dto/roomCollection";
-import {NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
+import {MAT_DIALOG_DATA, MatDialogContent, MatDialogRef} from "@angular/material/dialog";
+import {Observable} from "rxjs";
+import {Team} from "../dto/team";
+import {MatAutocomplete, MatAutocompleteTrigger} from "@angular/material/autocomplete";
+import {map, startWith} from "rxjs/operators";
 
 
 @Component({
@@ -33,57 +38,66 @@ import {NgForOf} from "@angular/common";
     MatMiniFabButton,
     MatSelect,
     MatOption,
-    NgForOf
+    NgForOf,
+    MatDialogContent,
+    AsyncPipe,
+    MatAutocomplete,
+    MatAutocompleteTrigger
   ],
   templateUrl: './editpi.component.html',
   styleUrl: './editpi.component.css'
 })
 export class EditpiComponent {
   pi: Pi | undefined
-  private piId?: number
+  roomFormControl = new FormControl(null)
+  filteredOptions: Observable<RoomDto[]> = new Observable<RoomDto[]>();
+  rooms: RoomDto[] = []
 
   piEditForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-    roomNo: new FormControl()
+    name: new FormControl(null, Validators.required),
+    roomNo: this.roomFormControl
   });
-  rooms: RoomDto[] = []
 
 
   constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
     private route: ActivatedRoute,
     private piService: PiService,
     private router: Router,
     private generalService: GeneralService,
-    private roomService: RoomService
+    private roomService: RoomService,
+    private dialogRef: MatDialogRef<EditpiComponent>
   ) {
-    this.route.params.subscribe(params=> {
-      this.piId =params['piId']
+      if (this.data) {
+        this.pi = data.pi
+      }
       this.fetchRooms();
-
-    });
-
-    piService.getPi(this.piId!)
-      .then((pi: Pi) => {this.pi = pi;})
-      .catch(_ => {
-        generalService.showSnackbar("No pi available", "OK")
-        router.navigate(['/pis'])
-      })
   }
 
   fetchRooms() {
     // Make an HTTP GET request to your backend API to fetch room numbers
     this.roomService.getAllRooms().then((rooms: RoomCollection) => {
-      this.rooms = rooms.rooms
+      this.rooms = rooms.rooms.filter(room => room.roomNo != this.pi?.roomNo)
+      this.setFormValues()
+      this.filteredOptions = this.roomFormControl.valueChanges
+        .pipe(
+          startWith<string | null>(''),
+          map(value => this._filter(value!!))
+        );
     });
   }
-
+  setFormValues() {
+    if (this.pi) {
+      this.piEditForm.controls['name'].setValue(this.pi.name);
+    }
+  }
 
   async submitEditPiForm() {
 
     const editPi: Pi = {
       id: this.pi?.id!!, // Assumes piId is non-null
       name: this.piEditForm.value.name ?? this.pi?.name,
-      roomNo: this.piEditForm.value.roomNo.roomNo ?? '',
+      roomNo: this.piEditForm.value.roomNo ?? '',
       status: this.pi?.status ?? "",
       dashboardName: this.pi?.dashboardName ?? "",
       macAddress: this.pi?.macAddress ?? "",
@@ -94,10 +108,19 @@ export class EditpiComponent {
     await this.piService.editPi(editPi)
       .then(_ => {
         this.generalService.showSnackbar('Pi data successfully updated', 'OK', {duration: 3000})
+        this.dialogRef.close()
       }).catch(_ => {
         this.generalService.showSnackbar('Error while updating Pi', 'OK')
       })
+  }
 
-    this.router.navigate(["/piManager"]);
+
+  displayFn(room?: any): string {
+    return room? room : undefined;
+  }
+
+  private _filter(value: string): RoomDto[] {
+    const filterValue = value.toLowerCase();
+    return this.rooms.filter(option => option.roomNo.toLowerCase().includes(filterValue));
   }
 }
