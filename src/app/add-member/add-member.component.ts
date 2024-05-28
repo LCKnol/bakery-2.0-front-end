1cbadbde8568e94238309a1a94479c5efa5ff2f4
Reem2810 <84314460+Reem2810@users.noreply.github.com>
2024-05-28 16:02:41 +0200

    add member functionality

--- src/app/add-member/add-member.component.ts
+++ src/app/add-member/add-member.component.ts
@@ -1,19 +1,17 @@
-import {Component, Inject} from '@angular/core';
-import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
-import {RoomDto} from "../dto/roomDto";
-import {MAT_DIALOG_DATA, MatDialogContent, MatDialogRef} from "@angular/material/dialog";
-import {RoomService} from "../services/room.service";
-import {Router} from "@angular/router";
-import {GeneralService} from "../services/general.service";
-import {RoomCollection} from "../dto/roomCollection";
-import {UserCollection} from "../dto/userCollection";
-import {UserService} from "../services/user.service";
-import {User} from "../dto/user";
-import {MatFormField, MatLabel} from "@angular/material/form-field";
-import {MatOption, MatSelect} from "@angular/material/select";
-import {MatButton} from "@angular/material/button";
-import {MatIcon} from "@angular/material/icon";
-import {NgForOf} from "@angular/common";
+import { Component, Inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
+import { MAT_DIALOG_DATA, MatDialogContent, MatDialogRef } from "@angular/material/dialog";
+import { UserService } from "../services/user.service";
+import { TeamService } from "../services/team.service";
+import { GeneralService } from "../services/general.service";
+import { UserCollection } from "../dto/userCollection";
+import { User } from "../dto/user";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatOption } from "@angular/material/autocomplete";
+import { MatSelect } from "@angular/material/select";
+import { MatButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { NgForOf } from "@angular/common";
 
 @Component({
   selector: 'app-add-member',
@@ -34,46 +32,42 @@ import {NgForOf} from "@angular/common";
 })
 export class AddMemberComponent {
   addMemberForm: FormGroup = new FormGroup({
-    name: new FormControl(),
-
+    members: new FormControl()
   });
 
-  userCollection : User[] = []
+  teamId: number | undefined;
+  userCollection: User[] = [];
 
-
-  constructor(@Inject(MAT_DIALOG_DATA) private data: any, private dialogRef: MatDialogRef<AddMemberComponent>,
-              private userService: UserService, private router: Router, private generalService: GeneralService) {
-    this.dialogRef.updateSize('40%')
-    this.fetchMembers()
-
+  constructor(
+    @Inject(MAT_DIALOG_DATA) private data: any,
+    private dialogRef: MatDialogRef<AddMemberComponent>,
+    private userService: UserService,
+    private teamService: TeamService,
+    private generalService: GeneralService
+  ) {
+    if (data) {
+      this.teamId = data.teamId;
+    }
+    this.dialogRef.updateSize('40%');
+    this.fetchMembers();
   }
-
 
   fetchMembers() {
     this.userService.getAllUsers().then((userCollection: UserCollection) => {
-      this.userCollection = userCollection.userCollection
+      this.userCollection = userCollection.userCollection;
     });
   }
 
-
   submitAddMemberForm() {
-    const user: User = {
-      id: -1,
-      firstName: this.addMemberForm.value.firstname,
-      lastName: this.addMemberForm.value.lastname,
-      email: this.addMemberForm.value.email,
-      password: this.addMemberForm.value.password,
-      isAdmin: this.addMemberForm.value.isAdmin,
-      teams: this.addMemberForm.value.teams
-    };
-    this.userService.addUser(user)
-      .then(token => {
-        this.generalService.showSnackbar("Room added successfully", "ok", {})
-        this.dialogRef.close()
+    const userId = this.addMemberForm.value.members;
+    this.teamService.assignUserToTeam(userId, this.teamId!!)
+      .then(_ => {
+        this.generalService.showSnackbar("Member added successfully", "ok", {});
+        this.dialogRef.close();
       })
       .catch(_ => {
-        this.generalService.showSnackbar("Adding Room failed", "ok", {});
+        this.generalService.showSnackbar("Adding Member failed", "ok", {});
+        this.dialogRef.close();
       });
   }
 }
-
