73c0538f95a0842d19a9487633a0b76ce1843a82
Reem2810 <84314460+Reem2810@users.noreply.github.com>
2024-05-28 16:04:58 +0200

    add team environment

--- src/app/team-manager/team-manager.component.ts
+++ src/app/team-manager/team-manager.component.ts
@@ -1,31 +1,39 @@
-import {AfterViewInit, Component, ViewChild} from '@angular/core';
+import { AfterViewInit, Component, ViewChild } from '@angular/core';
 import {MatButton, MatIconButton} from "@angular/material/button";
-import {MatCard} from "@angular/material/card";
+import { MatCard } from "@angular/material/card";
 import {
   MatCell,
   MatCellDef,
   MatColumnDef,
-  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
   MatHeaderRow,
   MatHeaderRowDef,
-  MatRow, MatRowDef, MatTable, MatTableDataSource
+  MatRow,
+  MatRowDef,
+  MatTable,
+  MatTableDataSource
 } from "@angular/material/table";
-import {MatIcon} from "@angular/material/icon";
-import {MatInput} from "@angular/material/input";
-import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
-import {MatPaginator} from "@angular/material/paginator";
-import {MatTab, MatTabGroup} from "@angular/material/tabs";
-import {MatToolbar} from "@angular/material/toolbar";
-import {NgForOf, NgIf} from "@angular/common";
-import {Router} from "@angular/router";
-import {MatDialog} from "@angular/material/dialog";
-import {TeamInfo} from "../dto/team.info";
-import {AddRoomComponent} from "../add-room/add-room.component";
-import {AddMemberComponent} from "../add-member/add-member.component";
-import {RemoveMemberComponent} from "../remove-member/remove-member.component";
-import {RemoveRoomComponent} from "../remove-room/remove-room.component";
-import {AddTeamComponent} from "../add-team/add-team.component";
-
+import { MatIcon } from "@angular/material/icon";
+import { MatInput } from "@angular/material/input";
+import { MatMenu, MatMenuItem, MatMenuTrigger } from "@angular/material/menu";
+import { MatPaginator } from "@angular/material/paginator";
+import { MatTab, MatTabGroup } from "@angular/material/tabs";
+import { MatToolbar } from "@angular/material/toolbar";
+import { NgForOf, NgIf } from "@angular/common";
+import { Router } from "@angular/router";
+import { MatDialog } from "@angular/material/dialog";
+import { TeamInfo } from "../dto/team.info";
+import { AddRoomComponent } from "../add-room/add-room.component";
+import { AddMemberComponent } from "../add-member/add-member.component";
+import { RemoveMemberComponent } from "../remove-member/remove-member.component";
+import { RemoveRoomComponent } from "../remove-room/remove-room.component";
+import { AddTeamComponent } from "../add-team/add-team.component";
+import { TeamService } from "../services/team.service";
+import {Team} from "../dto/team";
+import {TeamInfoCollection} from "../dto/TeamInfoCollection";
+import {RoomDto} from "../dto/roomDto";
+import {User} from "../dto/user";
 
 @Component({
   selector: 'app-team-manager',
@@ -40,7 +48,6 @@ import {AddTeamComponent} from "../add-team/add-team.component";
     MatHeaderRow,
     MatHeaderRowDef,
     MatIcon,
-    MatIconButton,
     MatInput,
     MatMenu,
     MatMenuItem,
@@ -53,26 +60,29 @@ import {AddTeamComponent} from "../add-team/add-team.component";
     MatToolbar,
     NgIf,
     MatMenuTrigger,
-    MatHeaderCellDef,
-    NgForOf
+    NgForOf,
+    MatIconButton,
+    MatHeaderCellDef
   ],
   templateUrl: './team-manager.component.html',
   styleUrls: ['./team-manager.component.css']
 })
 export class TeamManagerComponent implements AfterViewInit {
-  displayedColumns: string[] = [];
+  displayedColumns: string[] = ['name', 'members', 'rooms', 'action'];
   dataSource: MatTableDataSource<TeamInfo> = new MatTableDataSource<TeamInfo>();
-  teams?: TeamInfo[] = [];
-  filteredTeams?: TeamInfo[] = [];
+  teams: TeamInfo[] = [];
+  filteredTeams: TeamInfo[] = [];
 
   @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
 
-  constructor(private router: Router, public dialog: MatDialog) {
+  constructor(private router: Router, public dialog: MatDialog, private teamService: TeamService) {
     this.showAllTeams();
   }
 
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
   openAddTeamDialog() {
@@ -82,48 +92,43 @@ export class TeamManagerComponent implements AfterViewInit {
     });
   }
 
-  deleteTeam(id: string) {
-
-    this.showAllTeams();
+  deleteTeam(id: number) {
+    this.teamService.deleteTeam(id).then(() => {
+      this.showAllTeams();
+    });
   }
 
   private showAllTeams() {
-
-    const teams: TeamInfo[] = [
-      {
-        id: 1,
-        name: 'Team 1',
-        members: ['Alice', 'Bob'],
-        rooms: ['Room 101', 'Room 102']
-      },
-      {
-        id: 2,
-        name: 'Team 2',
-        members: ['Charlie', 'Dave'],
-        rooms: ['Room 201', 'Room 202']
-      }
-    ];
-    this.dataSource = new MatTableDataSource<TeamInfo>(teams);
-    this.displayedColumns = ['name', 'members', 'rooms', 'action'];
-    this.dataSource.paginator = this.paginator;
-    this.teams = teams;
-    this.filteredTeams = teams;
+    this.teamService.getAllTeamsInfo().then(res => {
+      console.log(res);
+      this.dataSource = new MatTableDataSource<TeamInfo>(res.teamInfoCollection);
+      this.displayedColumns = ['name', 'members', 'rooms', 'action'];
+      this.dataSource.paginator = this.paginator;
+      this.teams = res.teamInfoCollection;
+      this.filteredTeams = res.teamInfoCollection;
+    });
   }
+
+
 
   filterResults(text: string) {
     if (!text) {
-      this.dataSource = new MatTableDataSource<TeamInfo>(this.teams);
-      this.dataSource.paginator = this.paginator;
+      this.dataSource.data = this.teams;
+      if (this.paginator) {
+        this.dataSource.paginator = this.paginator;
+      }
       return;
     }
-    this.filteredTeams = this.teams?.filter(
-      team => team?.name.toLowerCase().includes(text.toLowerCase())
+    this.filteredTeams = this.teams.filter(
+      team => team.name.toLowerCase().includes(text.toLowerCase())
     );
-    this.dataSource = new MatTableDataSource<TeamInfo>(this.filteredTeams);
-    this.dataSource.paginator = this.paginator;
+    this.dataSource.data = this.filteredTeams;
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
   }
 
-  addRoom(teamId: string) {
+  addRoom(teamId: number) {
     const dialogRef = this.dialog.open(AddRoomComponent, {
       data: { teamId: teamId }
     });
@@ -132,16 +137,19 @@ export class TeamManagerComponent implements AfterViewInit {
     });
   }
 
-  removeRoom(teamId: string) {
+  removeRoom(teamId: number,rooms:RoomDto[]) {
     const dialogRef = this.dialog.open(RemoveRoomComponent, {
-      data: { teamId: teamId }
+      data: {
+        teamId: teamId,
+        rooms: rooms
+      }
     });
     dialogRef.afterClosed().subscribe(result => {
       this.showAllTeams();
     });
   }
 
-  addMember(teamId: string) {
+  addMember(teamId: number) {
     const dialogRef = this.dialog.open(AddMemberComponent, {
       data: { teamId: teamId }
     });
@@ -150,12 +158,18 @@ export class TeamManagerComponent implements AfterViewInit {
     });
   }
 
-  removeMember(teamId: string) {
+  removeMember(teamId: number,members:User[]) {
     const dialogRef = this.dialog.open(RemoveMemberComponent, {
-      data: { teamId: teamId }
+      data: {
+        teamId: teamId,
+        members: members
+      }
     });
     dialogRef.afterClosed().subscribe(result => {
       this.showAllTeams();
     });
   }
+
+
+
 }
