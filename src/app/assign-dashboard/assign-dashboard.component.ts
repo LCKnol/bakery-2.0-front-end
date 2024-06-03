68ba023391d8a647d9ce69a3a25bb6f9cf52bfed
Eduardo-ict <ej.vanbenthum@student.han.nl>
2024-06-03 16:02:42 +0200

    🚸updated assign dashboard to pi search field

--- src/app/assign-dashboard/assign-dashboard.component.ts
+++ src/app/assign-dashboard/assign-dashboard.component.ts
@@ -15,12 +15,16 @@ import {MatOption, MatSelect} from "@angular/material/select";
 import {DashboardDto} from "../dto/dashboardDto";
 import {DashboardService} from "../services/dashboard.service";
 import {DashboardCollection} from "../dto/dashboardCollection";
-import {NgForOf} from "@angular/common";
+import {AsyncPipe, NgForOf} from "@angular/common";
 import {Pi} from "../dto/pi";
 import {PiService} from "../services/pi.service";
 import {MatCard} from "@angular/material/card";
 import {MatIcon} from "@angular/material/icon";
 import {GeneralService} from "../services/general.service";
+import {Observable} from "rxjs";
+import {Team} from "../dto/team";
+import {map, startWith} from "rxjs/operators";
+import {MatAutocomplete, MatAutocompleteTrigger} from "@angular/material/autocomplete";
 
 @Component({
   selector: 'app-assign-dashboard',
@@ -41,16 +45,21 @@ import {GeneralService} from "../services/general.service";
     NgForOf,
     MatCard,
     MatIcon,
+    MatAutocompleteTrigger,
+    MatAutocomplete,
+    AsyncPipe,
   ],
   templateUrl: './assign-dashboard.component.html',
   styleUrl: './assign-dashboard.component.css'
 })
 export class AssignDashboardComponent {
-  dashboards?: DashboardDto[] = [];
+  dashboards: DashboardDto[] = [];
   pi: Pi | undefined
+  dashboardFormControl = new FormControl(null, [Validators.required])
+  filteredOptions: Observable<DashboardDto[]> = new Observable<DashboardDto[]>();
 
   assignDashboardForm: FormGroup = new FormGroup({
-    dashboardList: new FormControl('',[Validators.required]),
+    newDashboard: this.dashboardFormControl
   });
 
   constructor(@Inject(MAT_DIALOG_DATA) private data: any,private generalService: GeneralService, private dialogRef: MatDialogRef<AssignDashboardComponent>, private dashboardService: DashboardService, private piService: PiService,) {
@@ -59,6 +68,11 @@ export class AssignDashboardComponent {
     }
     dashboardService.getDashboards().then((dashboard: DashboardCollection) => {
       this.dashboards = dashboard.dashboards.filter(dashboard => dashboard.id !== this.pi?.dashboardId);
+      this.filteredOptions = this.dashboardFormControl.valueChanges
+        .pipe(
+          startWith<string | null>(''),
+          map(value => this._filter(value!!))
+        );
     });
   }
 
@@ -71,12 +85,25 @@ export class AssignDashboardComponent {
       status: this.pi?.status!!,
       dashboardName: this.pi?.dashboardName!!,
       roomNo: this.pi?.roomNo!!,
-      dashboardId: this.assignDashboardForm.value.dashboardList
+      dashboardId: this.assignDashboardForm.value.newDashboard.id
     };
-    this.piService.assignDashboard(newPi).then(r =>
-      this.dialogRef.close(true)).catch(_ => {
+
+    this.piService.assignDashboard(newPi).then(_ => {
+      this.dialogRef.close(true)
+      this.generalService.showSnackbar("Dashboard succesfully updated", "OK")
+    }).catch(_ => {
       this.generalService.showSnackbar("Error while assigning dashboard", "OK")
       this.dialogRef.close()
     })
+  }
+
+  displayFn(dashboard?: any): string {
+    return dashboard ? dashboard.dashboardName : undefined;
+  }
+
+  private _filter(value: string): DashboardDto[] {
+    const filterValue = value.toLowerCase();
+
+    return this.dashboards.filter(option => option.dashboardName.toLowerCase().includes(filterValue));
   }
 }
